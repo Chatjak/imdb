a4467d97476ae13d4754c1684829e0ff98341efb
Chatjak <mr.chatjak@gmail.com>
2023-08-23 08:58:49 +0700

    fetch data from tmdb api homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return;
+import Result from "@/components/Result";
+
+const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  const results = data.results;
+  return (
+    <div>
+      <Result results={results} />
+    </div>
+  );
 }
