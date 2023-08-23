567a9ea8302846524cb2f314a7948a709a9c1b5f
Chatjak <mr.chatjak@gmail.com>
2023-08-23 10:45:21 +0700

    edit name

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,8 @@ import SearchBox from "@/components/SearchBox";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDb Clobe",
-  description: "This is IMDb clobe",
+  title: "IMDb Clone",
+  description: "This is IMDb clone",
 };
 
 export default function RootLayout({ children }) {
